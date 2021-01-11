f12740ebfd71e1c0f59d7488dd87c9ab3a2172b6
Eric Fredericks <31228682+k-lergic@users.noreply.github.com>
2021-01-11 15:38:19 -0600

    license for example

--- examples/src/main/scala/org/kalergic/contextual/examples/akkastreams/PrintTweetHashtags.scala
+++ examples/src/main/scala/org/kalergic/contextual/examples/akkastreams/PrintTweetHashtags.scala
@@ -8,6 +8,34 @@ import com.typesafe.scalalogging.StrictLogging
 import org.kalergic.contextual.v0.contextualize._
 import org.kalergic.contextual.v0.correlation.{CorrelationId, CorrelationIdSupport}
 
+/*
+  This example is borrowed from, and modified from the following Akka Streams example, from the
+  Akka documentation.
+
+  https://doc.akka.io/docs/akka/current/stream/stream-quickstart.html#browser-embedded-example
+
+  Modifications Copyright (c) 2021, Eric J. Fredericks
+
+  License for Akka follows.
+  ---------------------------------------------------------------------------------------------
+
+  This software is licensed under the Apache 2 license, quoted below.
+
+  Copyright 2009-2018 Lightbend Inc. <https://www.lightbend.com>
+
+  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+  use this file except in compliance with the License. You may obtain a copy of
+  the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+  License for the specific language governing permissions and limitations under
+  the License.
+
+ */
 object PrintTweetHashtags extends App with StrictLogging {
 
   import akka.NotUsed
